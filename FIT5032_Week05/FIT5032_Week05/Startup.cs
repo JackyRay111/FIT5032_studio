3c04c352ccbe3a28401b7680a241d0262a866148
jacky <jlei0006@student.monash.edu>
2020-09-02 01:07:45 +1000

    Week05 task 1

---
+++ FIT5032_Week05/FIT5032_Week05/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_Week05.Startup))]
+namespace FIT5032_Week05
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
