ee99b23ca53ca8ed6ca76cd87f25ebbc7f365071
jacky <jlei0006@student.monash.edu>
2020-10-08 17:53:53 +1100

    week8 studio task

---
+++ FIT5032 Studio 8 - No API Key/FIT5032_Week08A/FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("jackyray111@gmail.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
